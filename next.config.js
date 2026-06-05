032af55df5955c325c0af013da0ab8986f347f92
root <root@vmi3271771.contaboserver.net>
2026-06-05 18:52:12 +0200

    chore: export static site for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   output: 'export',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '/bifrost',
+  basePath: '/Bifrost',
   trailingSlash: true,
   images: {
     unoptimized: true,
